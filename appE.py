7bab077cd80c6f97fdc40e57d62d95163312c6a2
ale5597 <ale5597@gmail.com>
2022-04-16 13:27:34 -0600

    Se hace la corrección del error de la aplicación para que no se caiga en ejecución. Hecho por Maria y Alejandro.

--- appE.py
+++ appE.py
@@ -11,29 +11,34 @@ if not cap.isOpened():
     raise IOError('No se puede abrir la cámara')
 
 while True:
-    ret,frame = cap.read() #Lee una imagen del video
-    result = DeepFace.analyze(frame,actions=['emotion'])
-    
-    ##############################################
-    #Hace el rectangulo alrededor de la cara
-    gray  = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray,1.1,4)
-    
-    for(x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h), (0,255,0),2)
-        
-    ##############################################
-    ##############################################
-    
-    font = cv2.FONT_HERSHEY_COMPLEX
-    cv2.putText(frame, 
-                result['dominant_emotion'],
-               (50,50), font, 3, (0,0,255), 2, cv2.LINE_4);
-    cv2.imshow('Demo video', frame)
-    
-    if cv2.waitKey(2) & 0xFF == ord('q'):
-        break
+    try:
+        ret,frame = cap.read() #Lee una imagen del video
+        result = DeepFace.analyze(frame,actions=['emotion'])
+
+        ##############################################
+        #Hace el rectangulo alrededor de la cara
+        gray  = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray,1.1,4)
+
+        for(x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h), (0,255,0),2)
+
+        ##############################################
+        ##############################################
+
+        font = cv2.FONT_HERSHEY_COMPLEX
+        cv2.putText(frame, 
+                    result['dominant_emotion'],
+                   (150,150), font, 3, (0,0,255), 2, cv2.LINE_4);
+        cv2.imshow('Demo video', frame)
+
+        if cv2.waitKey(2) & 0xFF == ord('q'):
+            break
+    except (ValueError) as err:
+        #print(err)
+        pass
         
 cap.release()
 cv2.destroyAllWindows()
+
     
